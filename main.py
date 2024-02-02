fbc57c115f915e308e58b353847900438fcb527b
Apllify <ralilahlou50@gmail.com>
2024-02-02 15:15:05 +0100

    Parser first draft

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import argparse
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="A python tool for finding molecule conformations.")
-    argparser.add_argument("prot_file", type = str, required  = True, metavar = ".prot file")
-    argparser.add_argument("peaks_file", type = str, required  = True, metavar = ".peaks file")
+    argparser.add_argument("prot_file", type = str, metavar = ".prot file")
+    argparser.add_argument("peaks_file", type = str, metavar = ".peaks file")
 
     args = argparser.parse_args()
 
     #TODO : call parser routines to get usable values
+    with open(args.prot_file, "r") as stream : 
+        prot_content = stream.read()
+    chem_shift_to_id, id_to_mol = parser.parse_prot(prot_content)
+    print(chem_shift_to_id)
+    print(id_to_mol)
+
+
+    with open(args.peaks_file, "r") as stream : 
+        peaks_content = stream.read()
+
+    p_assign = parser.parse_peaks(peaks_content, chem_shift_to_id, id_to_mol)
+    print(str(p_assign).replace(",", "\n"))
 
     #TODO : use parser results to build .dat file
 
--- parser.py
+++ parser.py
@@ -1,7 +1,19 @@
+import math
+from collections import OrderedDict
+
+
+#Type defs
 ChemShiftToID = dict[tuple[float, float], int]
 IDToMol = dict[int, str]
 
-TripletAssignment = list[ tuple[tuple[str, str, str], float] ]
+Mol = str | int # either ID or name
+TripletAssignment = list[ tuple[ Mol, Mol, Mol, float ] ]
+
+
+#GLOBAL DO NOT TOUCH
+CACHE_SIZE = 30
+lookup_cache = OrderedDict()
+
 
 def parse_prot(content : str) -> tuple[ChemShiftToID, IDToMol]:
     """
@@ -16,19 +28,27 @@ def parse_prot(content : str) -> tuple[ChemShiftToID, IDToMol]:
     lines = content.split("\n")
 
     for line in lines : 
-        terms = [term.strip for term in line.split(" ")]
+        terms = line.split()
+
+        #skip invalid lines
+        if line == "" or line[0] == "!" or len(terms) != 5 : 
+            continue
+
 
         #get all info from current line
-        id = int(terms[0])
-        chem_shift = float(terms[1])
-        err =  float(terms[2])
+        try : 
+            id = int(terms[0])
+            chem_shift = float(terms[1])
+            err =  float(terms[2])
 
-        mol_type = terms[3]
-        res_id = int(terms[4])
+            mol_type = terms[3]
+            res_id = int(terms[4])
+        except : 
+            continue
 
         #remember this molecule's name and ID for later
         mol_name =  f"{mol_type}_{res_id}"
-        IDToMol[id] = mol_name
+        id_to_mol[id] = mol_name
 
         #also store the chem shift of molecule if it is well defined
         if chem_shift != 999 : 
@@ -40,8 +60,85 @@ def parse_prot(content : str) -> tuple[ChemShiftToID, IDToMol]:
         
 
 
+def id_from_shift(chem_shift : float, chem_shift_to_id : ChemShiftToID) -> int : 
+    """
+    Determine the id of an atom from its chemical shift
+    using the value intervals established from the prot file 
+
+    Returns -1 when no valid id is found
+    """
+    global lookup_cache
+
+    cur_min_id = -1
+    cur_min =  math.inf
+
+    #check if we already computed this recently
+    if chem_shift in lookup_cache:
+        lookup_cache.move_to_end(chem_shift)
+        return lookup_cache[chem_shift]
+
+    #iterate through all intervals
+    for (lb, ub) in chem_shift_to_id.keys():
+        if chem_shift >= lb and chem_shift <= ub: 
+
+            dist = abs(chem_shift - (ub+lb)/2)
+            if  dist < cur_min:
+                cur_min_id = chem_shift_to_id[(lb, ub)]
+                cur_min = dist
+
+    #store result and return
+    if len(lookup_cache) >= CACHE_SIZE:
+        lookup_cache.popitem(last=False)
+
+    lookup_cache[chem_shift] = cur_min_id
+    
+    return cur_min_id
 
 
 
 def parse_peaks(content : str, chem_shift_to_id : ChemShiftToID, id_to_mol : IDToMol) -> TripletAssignment:
-    pass
+    lines = content.split("\n")
+#Assignmnet of rho to corresponding atoms follows scheme detailed in README.txt
+    triplet_assignment : TripletAssignment = []
+
+    for line in lines : 
+        #ignore comments
+        if line.strip() == "" or line.strip()[0] == "#":
+            continue
+
+        #get info from line
+        try : 
+            terms = line.split()
+            id = int(terms[0])
+            chem1, chem2, chem3 = map(float, terms[1:4]) #chem shifts
+            noe = float(terms[6])
+            m1, m2, m3 = map(int, terms[10:13]) #molecules
+        except : 
+            continue
+
+        #ignore negative peaks
+        if noe <= 0 : 
+            continue
+
+        #ignore lines that don't have 3 unique molecules / have a zero mol
+        uniques = set([chem1, chem2, chem3])
+        if len(uniques) <= 2 or 0 in uniques : 
+            continue 
+        
+        #find the atom IDs from their chem shifts
+        Ip = id_from_shift(chem1, chem_shift_to_id)
+        Iq = id_from_shift(chem2, chem_shift_to_id)
+        Ir = id_from_shift(chem3, chem_shift_to_id)
+        
+        if -1 in (Ip, Iq, Ir):
+            continue
+
+        #convert the ids to names
+        Np = id_to_mol.get(Ip)
+        Nq = id_to_mol.get(Iq)
+        Nr = id_to_mol.get(Ir)
+
+        #store the result
+        triplet_assignment.append( (Np, Nq, Nr, noe) )
+
+    return triplet_assignment
