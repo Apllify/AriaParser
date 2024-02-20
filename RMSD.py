e6378935c5a41e390ea23159c3b0fe9867642da9
Ba Cong Vu <41003815+souhhcong@users.noreply.github.com>
2024-02-20 16:48:07 +0100

    support calculating RMSD with multiple true aas

--- RMSD.py
+++ RMSD.py
@@ -14,7 +14,7 @@ class Atom:
         return self.name < other.name
     def __repr__(self):
         return f"Atom({self.name}, {self.coord})"
-    
+
 def get_aa_names_from_file(file):
     """
     Get all aa names in a top file
@@ -38,35 +38,64 @@ def get_aa_names_from_file(file):
                 aa_names.append(res_name)
     return aa_names
     
-# Calculate the RMSD
 def RMSDcalc(X0, Xr):
-   n = X0.shape[0]
-   X0bar = np.zeros((n,3))
-   Xrbar = np.zeros((n,3))
-   cmX0 = np.sum(X0, axis=0)/n
-   cmXr = np.sum(Xr, axis=0)/n
+    """
+    Calculate the RMSD between two numpy arrays
+    """
+    n = X0.shape[0]
+    X0bar = np.zeros((n,3))
+    Xrbar = np.zeros((n,3))
+    cmX0 = np.sum(X0, axis=0)/n
+    cmXr = np.sum(Xr, axis=0)/n
 
-   for i in range(n):
-      X0bar[i,:] = X0[i,:] - cmX0
-      Xrbar[i,:] = Xr[i,:] - cmXr
+    for i in range(n):
+        X0bar[i,:] = X0[i,:] - cmX0
+        Xrbar[i,:] = Xr[i,:] - cmXr
 
-   U,S,Vt = np.linalg.svd(np.dot(np.transpose(Xrbar), X0bar))
-   Q = np.dot(np.transpose(Vt), np.transpose(U))
-   Xrbar = np.dot(Xrbar, np.transpose(Q))
-   RMSD = np.linalg.norm(X0bar - Xrbar, ord='fro')/np.sqrt(n)
+    U,S,Vt = np.linalg.svd(np.dot(np.transpose(Xrbar), X0bar))
+    Q = np.dot(np.transpose(Vt), np.transpose(U))
+    Xrbar = np.dot(Xrbar, np.transpose(Q))
+    RMSD = np.linalg.norm(X0bar - Xrbar, ord='fro')/np.sqrt(n)
 
-   return X0bar, Xrbar, RMSD
+    return X0bar, Xrbar, RMSD
+
+# calculate the RMSD between 
+def RMSDcalc_res(res0, resr):
+    """
+    Calculate the RMSD between res0 (true residue) and resr (generated residue)
+    """
+    res0_atoms = [Atom(atom.name, atom.coord) for atom in res0]
+    resr_atoms = [Atom(atom.name, atom.coord) for atom in resr]
+
+    res0_atoms = sorted(res0_atoms)
+    for i in range(len(res0_atoms)):
+        # Change all 2-3 hydrogen atoms to 1-2 hydrogen atoms
+        if i < len(res0_atoms)-2 \
+        and res0_atoms[i+1].name[0] == 'H' and res0_atoms[i+1].name[-1] == '2' \
+        and res0_atoms[i+2].name[0] == 'H' and res0_atoms[i+2].name[-1] == '3' \
+        and res0_atoms[i].name != res0_atoms[i+1].name[:-1] + '1':
+            res0_atoms[i+1].name = res0_atoms[i+1].name[:-1] + '1'
+            res0_atoms[i+2].name = res0_atoms[i+2].name[:-1] + '2'
+        # Change all H atoms to HN atoms
+        if res0_atoms[i].name == 'H':
+            res0_atoms[i].name = 'HN'
+    res0_atoms = sorted(res0_atoms)
+
+    X0 = np.array([x.coord for x in res0_atoms])
+    Xr = np.array([x.coord for x in resr_atoms])
+    return RMSDcalc(X0, Xr)
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description="Calculating the RMSD between a generated pdb and residue position from pdb folder.")
-    argparser.add_argument("gen_pdb", type = str, metavar = "generated pdb")
+    argparser.add_argument("gen_pdb", type = str, metavar = "Generated pdb")
+    argparser.add_argument("num_res", type = int, metavar= "Number of residues to compare with")
 
     args = argparser.parse_args()
 
     #TOP parse
     aa_names = get_aa_names_from_file("data/aria.top")
-    res_atoms = {x: [] for x in aa_names}
-    aa_found = 0
+    residues = {x: [] for x in aa_names}
+    count_aa_found = 0
 
     # Parse downloaded pdb
     path = "data/pdb"
@@ -78,35 +107,27 @@ if __name__ == '__main__':
             for chain in model:
                 for residue in chain:
                     res_name = residue.get_resname()
-                    if res_name not in res_atoms:
+                    if res_name not in residues:
                         continue
-                    if len(res_atoms[res_name]) == 0:
-                        aa_found += 1
-                        res_atoms[res_name] = [Atom(atom.name, atom.coord) for atom in residue]
-        if aa_found == len(aa_names):
+                    if len(residues[res_name]) < args.num_res:
+                        count_aa_found += 1
+                        residues[res_name].append(residue)
+        if count_aa_found == len(aa_names) * args.num_res:
             break
 
     # Parse generated pdb
-    struct = pdbparser.get_structure(file.split('.')[0], args.gen_pdb)
+    struct = pdbparser.get_structure(args.gen_pdb.split('.')[0], args.gen_pdb)
     for model in struct:
         for chain in model:
-            for residue in chain:
+            for res_id, residue in enumerate(chain):
                 gen_res_name = residue.get_resname()
-                gen_atoms = sorted(Atom(atom.name, atom.coord) for atom in residue)
+                RMSD_avg = 0
+                if gen_res_name not in residues:
+                    print(f'For amino acid {gen_res_name} at residue {res_id+1}, not found in any pdb file')
+                    continue
 
-                true_atoms = sorted(res_atoms[gen_res_name])
-                for i in range(len(true_atoms)):
-                    # Change all 2-3 hydrogen atoms to 1-2 hydrogen atoms
-                    if i < len(true_atoms)-2 \
-                    and true_atoms[i+1].name[0] == 'H' and true_atoms[i+1].name[-1] == '2' \
-                    and true_atoms[i+2].name[0] == 'H' and true_atoms[i+2].name[-1] == '3' \
-                    and true_atoms[i].name != true_atoms[i+1].name[:-1] + '1':
-                        true_atoms[i+1].name = true_atoms[i+1].name[:-1] + '1'
-                        true_atoms[i+2].name = true_atoms[i+2].name[:-1] + '2'
-                    if true_atoms[i].name == 'H':
-                        true_atoms[i].name = 'HN'
-                true_atoms = sorted(res_atoms[gen_res_name])
-                X0 = np.array([x.coord for x in true_atoms])
-                Xr = np.array([x.coord for x in gen_atoms])
-                X0bar, Xrbar, RMSD = RMSDcalc(X0, Xr)
-                print(f'RMSD: {RMSD}')
+                for true_residue in residues[gen_res_name]:
+                    X0bar, Xrbar, RMSD = RMSDcalc_res(true_residue, residue)
+                    RMSD_avg += RMSD
+                RMSD_avg /= len(residues[gen_res_name])
+                print(f'For amino acid {gen_res_name} at residue {res_id+1}, average RMSD = {RMSD_avg}')
